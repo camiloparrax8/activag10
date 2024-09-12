bc9fc29af2bcedd5fe3f5c758d4e727ad2781830
PC-HP <116674187+FullstackBK@users.noreply.github.com>
2024-09-12 17:55:43 -0500

    headertopfour

--- layout/headers/headertopfour.jsx
+++ layout/headers/headertopfour.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { getLanguage, setLanguage, getTranslation } from '../../utils/i18n';
 
 const HeaderTopFour = () => {
+
+    const language = getLanguage();
+    console.log(language);
     return (
         <div className="header-top-area style-4 d-none d-lg-block">
             <div className="container">
@@ -20,7 +24,7 @@ const HeaderTopFour = () => {
                                     <i className="fas fa-envelope-open"></i>
                                 </div>
                                 <div className="meta-item-text">
-                                    <p><a href="mailto:info@gardomia.com">info@gardomia.com</a></p>
+                                    <p><a href="mailto:info@gardomia.com">info@activag10.com</a></p>
                                 </div>
                             </div>
                         </div>
@@ -28,13 +32,15 @@ const HeaderTopFour = () => {
                     <div className="header-top-right">
                         <div className="meta-items">
                             <div className="meta-item">
-                                <div className="meta-item-icon">
-                                    <i className="fas fa-clock"></i>
-                                </div>
-                                <div className="meta-item-text">
-                                    <p>Mon - Fri: 9:00 - 19:00<span className="d-none d-xl-inline-block">/ Closed on 
-                                        <strong> Weekends</strong></span>
-                                    </p>
+                                <div className="dropdown filter-item-btn">
+                                    <button className="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                        <i className="fal fa-globe"></i>{getTranslation(language, `language.${language}`)}<i className="fal fa-angle-down"></i>
+                                    </button>
+                                    <ul className="dropdown-menu">
+                                        <li><button className="dropdown-item" onClick={() => setLanguage('es')}>{getTranslation(language, 'language.es')}</button></li>
+                                        <li><button className="dropdown-item" onClick={() => setLanguage('en')}>{getTranslation(language, 'language.en')}</button></li>
+                                        <li><button className="dropdown-item" onClick={() => setLanguage('em')}>{getTranslation(language, 'language.em')}</button></li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
