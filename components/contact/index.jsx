0ef0242aff9d21b0d753bb0ee0c548404a5bb5ff
PC-HP <116674187+FullstackBK@users.noreply.github.com>
2024-09-16 10:21:12 -0500

    footer en contact

--- components/contact/index.jsx
+++ components/contact/index.jsx
@@ -4,6 +4,7 @@ import HeaderOne from '../../layout/headers/header';
 import Breadcrumb from '../common/breadcrumb';
 import ContactFormSection from './contact-form-section';
 import ContactMapSection from './contact-map-section';
+import FooterFour from '../../layout/footers/footerfour';
 
 
 const index = () => {
@@ -13,7 +14,7 @@ const index = () => {
             <Breadcrumb breadTitle={'contact us'} breadSubtitle={'Provides hassle-free backyard transformation'} breadHome={'Home'} breadMenu={'contact'} />
             <ContactFormSection />
             <ContactMapSection />
-            <FooterOne />
+            <FooterFour />
         </main>
     );
 };
