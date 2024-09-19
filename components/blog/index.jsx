f5bc1b03fd7b716f1ebc065cf3c49e4ecd8a0f0f
jemeye <jeni.medina2000@gmail.com>
2024-09-18 20:23:28 -0500

    refactor change footer

--- components/blog/index.jsx
+++ components/blog/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import FooterOne from '../../layout/footers/footer';
 import HeaderOne from '../../layout/headers/header';
 import Breadcrumb from '../common/breadcrumb';
 import BlogSectionMain from './blog-section-main';
+import FooterFour from '../../layout/footers/footerfour';
 
 
 const index = () => {
@@ -11,7 +11,7 @@ const index = () => {
             <HeaderOne />
             <Breadcrumb breadTitle={'blog & insights'} breadSubtitle={'Provides hassle-free backyard transformation'} breadHome={'Home'} breadMenu={'Blog'} />
             <BlogSectionMain />
-            <FooterOne />
+            <FooterFour />
         </main>
     );
 };
